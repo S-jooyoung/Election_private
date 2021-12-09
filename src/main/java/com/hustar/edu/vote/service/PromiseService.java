985548b6d1edfd3a85b18656ddc025aead34be8d
dongchaebaek <ehdco8988@gmail.com>
2021-12-09 18:29:56 +0900

    공약글쓰기 DB연결완료

---
+++ src/main/java/com/hustar/edu/vote/service/PromiseService.java
@@ -0,0 +1,20 @@
+package com.hustar.edu.vote.service;
+
+import com.hustar.edu.vote.dao.BoardDTO;
+import com.hustar.edu.vote.dao.tb_user;
+import com.hustar.edu.vote.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PromiseService {
+
+    @Autowired
+    private BoardMapper boardMapper;
+
+    @Transactional
+    public void insertBoard(BoardDTO board) {
+        boardMapper.insertBoard(board);
+    }
+}
